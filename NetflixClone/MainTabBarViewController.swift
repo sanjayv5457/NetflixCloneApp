e1d13b1356418f1fbf8a70a6770f7751f0726c27
sanjay <sdvaishnav44@gnail.com>
2025-07-14 15:56:15 +0530

    Tabbar done

--- NetflixClone/MainTabBarViewController.swift
+++ NetflixClone/MainTabBarViewController.swift
@@ -7,11 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainTabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemYellow
+        
+        let vc1 = UINavigationController(rootViewController: HomeViewController())
+        let vc2 = UINavigationController(rootViewController: UpcomingViewController())
+        let vc3 = UINavigationController(rootViewController: SearchViewController())
+        let vc4 = UINavigationController(rootViewController: DownloadsViewController())
+
+        
+        
+        vc1.tabBarItem.image = UIImage(systemName: "house")
+        vc2.tabBarItem.image = UIImage(systemName: "play.circle")
+        vc3.tabBarItem.image = UIImage(systemName: "magnifyingglass")
+        vc4.tabBarItem.image = UIImage(systemName: "arrow.down.to.line")
+        
+        
+        vc1.title = "House"
+        vc2.title = "Coming Sonn"
+        vc3.title = "Top Search"
+        vc4.title = "Downloads"
+
+
+        setViewControllers([vc1, vc2, vc3, vc4], animated: true)
     }
 
 
