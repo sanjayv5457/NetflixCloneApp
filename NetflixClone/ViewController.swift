e3a2cd67bbe848e5e5725e93d2821bd8eb228ccd
sanjay <sdvaishnav44@gnail.com>
2025-07-14 15:18:57 +0530

    Initial Commit

---
+++ NetflixClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetflixClone
+//
+//  Created by iMac on 14/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
