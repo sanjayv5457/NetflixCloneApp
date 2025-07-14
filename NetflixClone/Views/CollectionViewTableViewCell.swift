359216b62620391a912b917c5da7856d40615e84
sanjay <sdvaishnav44@gnail.com>
2025-07-14 17:10:18 +0530

    In TableVIew add CollectionView and header size add

---
+++ NetflixClone/Views/CollectionViewTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CollectionViewTableViewCell.swift
+//  NetflixClone
+//
+//  Created by iMac on 14/07/25.
+//
+
+import UIKit
+
+class CollectionViewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
